6216b9738b65790865c480e0b99ce5d175846073
Alex <oleksandr.humarov@gmail.com>
2025-05-13 19:00:55 +0300

    gpt set

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-car = {'brand': 'bmw', 'model': '871', 'year': 2018}
-car['color'] = 'black'
-car_copy = car.copy()
-car_copy.popitem()
-print(car)
-car_copy.clear()
-print(car, car_copy)
-=======
-my_dict = {'name': 'Alice', 'age': '30', 'city': 'Paris'}
-print(my_dict['city'])
-my_dict['age'] = 31
-my_dict['job'] = 'Engineer'
-del my_dict['name']
-print(my_dict)
->>>>>>> 800005252a38471b2d974ce6230ba96ee58807a1
+a = [1, 2, 3, 4]
+b = [3, 4, 5, 6]
+print(list(set(a).difference(b)))
