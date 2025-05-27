1356cf179563b0b1603456d0aa27ee59e4094f4c
Alex <oleksandr.humarov@gmail.com>
2025-05-27 15:36:00 +0300

    function

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-set1 = {1, 2, 3, 4}
-set2 = {3, 4, 5, 6}
-set1 = set1.intersection(set2)
-print(set1)
+def merge_list_to_dict(list_1, list_2):
+    return dict(zip(list_1, list_2))
+
+
+list_1 = ['one', 'two', 'three']
+list_2 = [1, 2, 3]
+
+result = merge_list_to_dict(list_1, list_2)
+print(result)
