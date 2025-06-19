b2a463b5adc6bdbde7aedf1f7f1edabb473cebe7
Alex <oleksandr.humarov@gmail.com>
2025-06-19 15:36:27 +0300

    traning

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def merge_list_to_dict(list_1, list_2):
-    return dict(zip(list_1, list_2))
+def merge_list_to_dict(list1, list2):
+    list_to_dict = list(zip(list1, list2))
+    print(list_to_dict)
 
 
-list_1 = ['one', 'two', 'three']
-list_2 = [1, 2, 3]
-
-result = merge_list_to_dict(list_1, list_2)
-print(result)
+name = ['Alex', 'Jon', 'Leya']
+age = [10, 15, 20]
+merge_list_to_dict(name, age)
